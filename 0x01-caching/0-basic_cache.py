e2699c44fea28e0429527feb071acb91806857f9
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-18 17:45:15 +0300

    put and get methods

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Basic dictionary 
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Inherits from the main class
+    Attributes:
+              key, item
+    Methods:
+            put(), get() 
+    """  
+    def put(self, key, item):
+        """
+        Assigns key to item
+        """
+        if key is not None and item  is None:
+            pass
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        """
+        Displays the item associated with a key.
+        The key should be present in the cache and not None
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
+        
+         
