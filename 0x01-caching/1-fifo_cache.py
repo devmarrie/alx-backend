eaf8dabdc3857b01f08b2b7535e1a9f80d37c088
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-18 21:38:16 +0300

    using iter() and next to produce the keys

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+FIFO caching 
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            pass
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            keyitr = next(iter(self.cache_data))
+            print(f'DISCARD:{keyitr}')
+            del self.cache_data[keyitr]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None and key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
