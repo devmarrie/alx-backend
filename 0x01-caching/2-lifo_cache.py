ad7e84f9b5336af61a51dd35792495ed61e6ae18
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-18 23:03:49 +0300

    last in

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+LIFO Caching 
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+    
+    def put(self, key, item):
+        if key is None or item is None:
+            pass
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            last_key = list(self.cache_data.keys())[-1]
+            del self.cache_data[last_key]
+            print(f'DISCARD: {last_key}')
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
