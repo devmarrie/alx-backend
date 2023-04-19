06c8db07476e12e182959c7e4b6b787511a38131
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-20 00:31:05 +0300

    corrections

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- LRU Caching 
+ LRU Caching
 """
 from base_caching import BaseCaching
 
@@ -8,26 +8,32 @@ from base_caching import BaseCaching
 class LRUCache(BaseCaching):
     """
     The super method.
-    LRU we need to store recent values 
+    LRU we need to store recent values
     """
     def __init__(self):
         super().__init__()
         self.lru = []
 
     def put(self, key, item):
+        """
+        Altering the least recently used
+        """
         if key is None or item is None:
-            pass           
+            pass
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            latest = self.lru.pop()
+            latest = self.lru[0]
             del self.cache_data[latest]
+            del self.lru[0]
             print(f'DISCARD:{latest}')
         self.cache_data[key] = item
         self.lru.append(key)
 
     def get(self, key):
-        if key is None and key  not in self.cache_data:
-            return None
-        self.lru.remove(key)
-        self.lru.append(key)
-        return self.cache_data[key]
-        
+        """
+        Fetching the LRU
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.lru[self.lru.index(key)]
+            self.lru.append(key)
+            return self.cache_data[key]
+        return None
