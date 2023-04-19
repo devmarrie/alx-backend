fb22fae7bcec011066eed2ce4bfa4ba0625386b5
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-19 22:55:12 +0300

    comments

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,10 +6,19 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """
+    Methods:
+            put: to change the elements
+            get: assign a key to a value
+    """
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
+        """
+        Caches the data and removes on a FIFO basis
+        The first one in first one out.
+        """
         if key is None or item is None:
             pass
         if len(self.cache_data) >= self.MAX_ITEMS:
@@ -19,6 +28,10 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Assign key to a value.
+        Key to be present in cache_data
+        """
         if key is None and key not in self.cache_data:
             return None
         else:
