f6d7d8acafb31e162c8ca330bee59d65d7079f6d
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-19 23:51:19 +0300

    docstrings

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 """
 LIFO Caching
+Create a class LIFOCache that inherits from 
+BaseCaching and is a caching system
 """
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    """
+    Arguments:
+              key, item
+    Methods:
+            get(), post()
+    """
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
+        """
+        Alters the cache value
+        Based on the LastIn FistOut basis.
+        """
         if key is None or item is None:
             pass
         if len(self.cache_data) >= self.MAX_ITEMS:
@@ -19,6 +31,9 @@ class LIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Get the values associated with the key
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
