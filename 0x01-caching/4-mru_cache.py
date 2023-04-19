cd09b8a33e5019369bebc81187fb45cffb8243dc
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-20 00:39:56 +0300

    mru

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- LRU Caching 
+ MRU Caching
 """
 from base_caching import BaseCaching
 
@@ -8,26 +8,32 @@ from base_caching import BaseCaching
 class MRUCache(BaseCaching):
     """
     The super method.
-    MRU we need to store recent values 
+    MRU we need to store recent values
     """
     def __init__(self):
         super().__init__()
         self.mru = []
 
     def put(self, key, item):
+        """
+        Altering the least recently used
+        """
         if key is None or item is None:
-            pass           
+            pass
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            latest = self.mru.pop(-1)
+            latest = self.mru[-1]
             del self.cache_data[latest]
+            del self.mru[-1]
             print(f'DISCARD:{latest}')
         self.cache_data[key] = item
         self.mru.append(key)
 
     def get(self, key):
-        if key is None and key  not in self.cache_data:
-            return None
-        self.mru.remove(key)
-        self.mru.append(key)
-        return self.cache_data[key]
-        
+        """
+        Fetching the mru
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.mru[self.mru.index(key)]
+            self.mru.append(key)
+            return self.cache_data[key]
+        return None
