1724b2cb1a97d7dd1a9cfdb4744551750e561d8c
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-25 22:15:01 +0300

    babel def

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    Defining various class variables for babel.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@app.route("/", strict_slashes=False)
+def home() -> str:
+    """
+    Diaplays 1-index.html
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
