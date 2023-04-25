c8f5c31c0eaf83edc6e71241cab519c6e5949d01
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-25 21:54:11 +0300

    page diaplay

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home() -> str:
+    """
+    Displays 0-index.html
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
