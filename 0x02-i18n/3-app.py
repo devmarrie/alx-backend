28ae6de193f7d71a63491c9de298c3393ff9b20c
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-26 22:27:12 +0300

    cont

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+from flask_babel import Babel, gettext as _
+from flask import Flask, render_template, request
+
+
+class Config(object):
+    """
+    Defining various class variables for babel.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determines the locale used in language translation
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def home() -> str:
+    """
+    Displays 3-index.html
+    Translates to user's language 
+    Then passes it to the web
+    """
+    title = _('home_title')
+    header = _('home_header')
+    return render_template("3-index.html", title=title, header=header)
+
+
+if __name__ == "__main__":
+    app.run()
