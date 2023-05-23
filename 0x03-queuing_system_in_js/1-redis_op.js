d0620f502c042eee657dd01162d876247abf7ce6
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-23 23:25:07 +0300

    shorter statement

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -6,7 +6,8 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-client.on('error', (err) => console.log(`Redis client not connected to the server:${err}`));
+client.on('error', (err) => console.log(
+    `Redis client not connected to the server:${err}`));
 
 function setNewSchool(schoolName, value) {
     client.set(schoolName, value, print);
