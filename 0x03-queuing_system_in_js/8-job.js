c5c1f15fdf08144837a4f8c1e1dcd8a45d86caa0
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-24 11:11:40 +0300

    final try

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,23 +1,22 @@
 function createPushNotificationsJobs(jobs, queue) {
-  if(Array.isArray(jobs)) {
+    if (!Array.isArray(jobs)) {
+      throw Error('Jobs is not an array');
+    }
+  
     jobs.forEach((i) => {
-      const job = queue.create(
-        'push_notification_code_3', i).save(function(err) {
-           if(!err) console.log(
-            `Notification job created: ${job.id}`
-           );
-        });
-
-        job.on('complete', function() {
-            console.log(`Notification job ${job.id} completed`)
-        }).on('failed', function(err) {
-            console.log(`Notification job ${job.id} failed: ${err}`)
-        }).on('progress', function(progress) {
-            console.log(`Notification job ${job.id} ${progress} complete`);
-        });
-    })
-  } else {
-    throw  Error('Jobs is not an array');
+      let job = queue.create('push_notification_code_3', i);
+  
+      job.on('complete', function() {
+        console.log(`Notification job ${job.id} completed`);
+      }).on('failed', function(error) {
+        console.log(`Notification job ${job.id} failed: ${error}`);
+      }).on('progress', function(progress, data) {
+        console.log(`Notification job ${job.id} ${progress}% complete`);
+      });
+      job.save((error) => {
+        if (!error) console.log(`Notification job created: ${job.id}`);
+      });
+    });
   }
-}
-module.exports = createPushNotificationsJobs()
+  
+  module.exports = createPushNotificationsJobs;
