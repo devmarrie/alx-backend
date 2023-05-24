67ca2f6a8d566c85f391b2e501ddfc4f273715f8
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-24 23:35:18 +0300

    well well

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -1,80 +1,81 @@
-import { create } from 'domain';
 import { createClient } from 'redis';
-const kue = require('kue');
-const util = require('util');
-const express = require('express')
+import { createQueue } from 'kue';
+import { promisify } from 'util';
+import express from 'express';
 
-const port = 1245;
-const redis_port = 6379;
-const app = express()
-const reservationEnabled = true;
-const queue = kue.createQueue();
+//create redis client
+const redisClient = createClient();
 
-
-const client = createClient(redis_port);
-
-client.on('connect', function() {
-    console.log('Redis client connected to the server');
+redisClient.on('connect', function() {
+  console.log('Redis client connected to the server');
 });
 
-client.on('error', function(err) {
-    console.log(`Redis client not connected to the server ${err}`);
+redisClient.on('error', function (err) {
+  console.log(`Redis client not connected to the server: ${err}`);
 });
 
+//promisify client.get function
+const asyncGet = promisify(redisClient.get).bind(redisClient);
 
-function reserveSeat(number, ) {
-  client.set('available_seats', number);
+function reserveSeat(number) {
+  redisClient.set('available_seats', number);
 }
 
-const asyncGet = util.promisify(client.get).bind(client);
-    
 async function getCurrentAvailableSeats() {
   const seats = await asyncGet('available_seats');
   return seats;
 }
 
-app.get('/available_seats', (req, res) => {
-    const availble = getCurrentAvailableSeats()
-    res.json({"numberOfAvailableSeats": availble})
+let reservationEnabled = true;
+
+//create Kue queue
+const queue = createQueue();
+
+//create express app
+const app = express();
+
+app.get('/available_seats', async function (req, res) {
+  const availableSeats = await getCurrentAvailableSeats();
+  res.json({"numberOfAvailableSeats": availableSeats});
 });
 
-app.get('/reserve_seat', (req, res) => {
-    if (!reservationEnabled) {
-        res.json({"status": "Reservation are blocked"});
-        return;
-      }
-      const job = queue.create('reserve_seat', {'seat': 1}).save((error) => {
-        if (error) {
-          res.json({"status": "Reservation failed"});
-          return;
-        } else {
-          res.json({"status": "Reservation in process"});
-          job.on('complete', function () {
-          console.log(`Seat reservation job ${job.id} completed`);
-          });
-          job.on('failed', function(error) {
-            console.log(`Seat reservation job ${job.id} failed: ${error}`);
-          });
-        }
+app.get('/reserve_seat', function (req, res) {
+  if (!reservationEnabled) {
+    res.json({"status": "Reservation are blocked"});
+    return;
+  }
+  const job = queue.create('reserve_seat', {'seat': 1}).save((error) => {
+    if (error) {
+      res.json({"status": "Reservation failed"});
+      return;
+    } else {
+      res.json({"status": "Reservation in process"});
+      job.on('complete', function () {
+      console.log(`Seat reservation job ${job.id} completed`);
       });
-    });
+      job.on('failed', function(error) {
+        console.log(`Seat reservation job ${job.id} failed: ${error}`);
+      });
+    }
+  });
+});
 
-app.get('/process', (req,res) => {
-    res.json({ "status": "Queue processing" });
+app.get('/process', function (req, res) {
+    res.json({"status": "Queue processing"});
     queue.process('reserve_seat', async function(job, done) {
-        const available = Number(await getCurrentAvailableSeats());
-        if (available === 0) {
-            reservationEnabled = false;
-            done(Error('Not enough seats available'));
-        } else {
-            reserveSeat(available - 1);
-            done();
-        }
+	const seat = Number(await getCurrentAvailableSeats());
+	if (seat === 0) {
+	    reservationEnabled = false;
+	    done(Error('Not enough seats available'));
+	} else {
+	    reserveSeat(seat - 1);
+	    done();
+	}
     });
 });
 
+const port = 1245;
 app.listen(port, () => {
-    console.log(`Application listening on port ${port}`)
+    console.log(`app is listening http://localhost:${port}`);
 });
-
-reserveSeat(100);
+reserveSeat(50);
